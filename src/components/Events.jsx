4dba0a854d20e45b93a2aec85804efb0a7568a50
Robiul Islam <robiulislamrobi0874@gmail.com>
2025-01-15 10:47:29 +0600

    choose us section done

---
+++ src/components/Events.jsx
@@ -0,0 +1,15 @@
+
+
+const Events = () => {
+	return (
+		<div className="pt-20 pb-12">
+			{/* header */}
+			<div>
+				<div> </div>
+				<button></button>
+			</div>
+		</div>
+	);
+};
+
+export default Events;
